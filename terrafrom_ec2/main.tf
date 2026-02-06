7d1a1a14cf1f7f28b0714e7a9a0f62396e5d336d
Saravana011 <161407668+Saravana011@users.noreply.github.com>
2026-02-06 17:10:41 +0530

    Delete terrafrom_ec2 directory

--- terrafrom_ec2/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-#adding new line to check in the web git
-}
--- terrafrom_ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.my_ec2.id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.my_ec2.public_ip
-}
